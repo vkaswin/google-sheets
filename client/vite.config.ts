b37f3bf0a9e70f09fa16625079ecb90d2f76c921
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-07-20 18:06:03 +0530

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,5 +10,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src/"),
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/google-sheets" : "/",
   server: { port: 3000 },
 });
