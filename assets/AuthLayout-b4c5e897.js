618864526c6883cf2d0e1f032e59591c6866bf6e
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-12-15 15:53:03 +0530

    Updates

---
+++ assets/AuthLayout-b4c5e897.js
@@ -0,0 +1,1 @@
+import{j as e,O as t}from"./index-8f004813.js";const r=()=>e.jsx("div",{className:"flex items-center justify-center min-h-full bg-[#e3edfd]",children:e.jsx(t,{})});export{r as default};
